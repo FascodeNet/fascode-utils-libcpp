4088fcf2c33ecf6724953c13950b4ca10ae9a51f
kokkiemouse <kokkiemouse@fascode.net>
2020-12-06 22:53:21 +0900

    added doxygen
    Signed-off-by: kokkiemouse <kokkiemouse@fascode.net>

--- include/custom_exec.hpp
+++ include/custom_exec.hpp
@@ -1,11 +1,27 @@
 #pragma once
+/***
+ * @file custom_exec.hpp
+ * @brief original fork exec
+ * 
+ */
 #include <vector>
 #include <string>
 #include <wait.h>
 #include <stdlib.h>
 #include <initializer_list>
 #include <unistd.h>
+/**
+ * @namespace FascodeUtil
+ * @brief FascodeUtil
+ * @details FascodeUtil
+*/
 namespace FascodeUtil{
+    /**
+     * @fn int custom_exec_v(std::vector<std::string> args)
+     * @brief custom exec
+     * @param args command args
+     * @return exit code
+     */
     int custom_exec_v(std::vector<std::string> args);
     template<class... T> 
     int custom_exec(T... args);
